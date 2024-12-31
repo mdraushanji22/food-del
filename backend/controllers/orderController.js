8cc9c6e55d5d21bd7896ba93d79fe10b307c72e6
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2024-12-31 23:08:13 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for fronend
 
 const placeOrder = async (req, res) => {
-  const frontend_url = " http://localhost:5173";
+  const frontend_url = " food-del-bay.vercel.app";
 
   try {
     const newOrder = new orderModel({
