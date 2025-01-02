e03dd4f96281c3ea77d62c864130e6f56317d738
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2025-01-02 17:51:22 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,7 +4,7 @@ import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
-export const url = 'food-del-bay.vercel.app'
+export const url = "https://food-deli-backend-ilpi.onrender.com"
 export const assets ={
     logo,
     add_icon,
