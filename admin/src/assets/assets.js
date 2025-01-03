4edc7c4e655740f6d081becfd7b9e8cc94fee4f0
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2025-01-04 02:59:52 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,6 +4,7 @@ import order_icon from "./order_icon.png";
 import profile_image from "./profile_image.png";
 import upload_area from "./upload_area.png";
 import parcel_icon from "./parcel_icon.png";
+export const url = "https://food-del-backend-vy4z.onrender.com";
 
 export const assets = {
   logo,
@@ -13,4 +14,4 @@ export const assets = {
   upload_area,
   parcel_icon,
 };
-export const url = "https://food-del-backend-vy4z.onrender.com";
+
