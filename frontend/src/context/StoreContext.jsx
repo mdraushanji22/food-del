f605057d6a36f76c3acdd92c79264d086ca94acd
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2025-01-03 19:21:50 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const StoreContext = createContext(null);
 const StoreContextprovider = (props) => {
-   const url = "https://food-deli-backend-ilpi.onrender.com";
+   const url = "http://localhost:4000";
      const [food_list, setFoodList] = useState([]);
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
