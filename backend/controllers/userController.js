4484f3c3c664444cf81b4bf24402d3188cad3e26
mdraushanji22 <mdraushanji22@gmail.com>
2025-01-04 00:27:26 +0530

    updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
+const bcrypt = require("bcryptjs");
 
 // Login user
 const loginUser = async (req, res) => {
