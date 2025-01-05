d26c0d0b8870daf6bbeb4c4c0b6316f6b32e0324
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2025-01-05 22:12:59 +0530

    Update Orders.jsx

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -12,7 +12,7 @@ const Orders = ({ url }) => {
     const response = await axios.get(url + "/api/order/list");
     if (response.data.success) {
       setOrders(response.data.data);
-      // console.log(response.data.data);
+       console.log(response.data.data);
     } else {
       toast.error("Error");
     }
@@ -26,9 +26,9 @@ const Orders = ({ url }) => {
       await fetchAllOrders();
     }
   };
-  // useEffect(() => {
-  //   fetchAllOrders();
-  // }, []);
+   useEffect(() => {
+     fetchAllOrders();
+   }, []);
   return (
     <div className="order add">
       <h3>Order Page</h3>
